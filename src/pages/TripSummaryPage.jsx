bba802d2e64708668502725cc32d170d890e70f3
thomas <thomas@sibilly.com>
2025-07-15 10:12:50 -0700

    arrows increased in size

--- src/pages/TripSummaryPage.jsx
+++ src/pages/TripSummaryPage.jsx
@@ -16,7 +16,12 @@ import {
   Container,
 } from "@mantine/core";
 import { Carousel } from "@mantine/carousel";
-import { IconBubbleFilled, IconMap2 } from "@tabler/icons-react";
+import {
+  IconBubbleFilled,
+  IconMap2,
+  IconArrowRight,
+  IconArrowLeft,
+} from "@tabler/icons-react";
 // TODO: DELETE THIS AFTER BACKEND  IS CONNECTED
 import { LoremIpsum } from "react-lorem-ipsum";
 import { useDisclosure } from "@mantine/hooks";
@@ -126,6 +131,8 @@ const TripSummaryPage = () => {
               slideGap={{ base: 0, sm: "md" }}
               slideSize="33.3333%"
               emblaOptions={{ loop: true, align: "start" }}
+              nextControlIcon={<IconArrowRight size={30} />}
+              previousControlIcon={<IconArrowLeft size={30} />}
             >
               {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
                 <Carousel.Slide key={item}>
