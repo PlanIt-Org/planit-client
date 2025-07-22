7a3b8372c6fd60470315b29c3434cb855d71840d
Josh Cesar Pierre <jcesarpierre@jcesarp-ltmuaun.internal.salesforce.com>
2025-07-22 13:58:23 -0700

    adding location to trips work

--- src/components/DragDropLocations.jsx
+++ src/components/DragDropLocations.jsx
@@ -1,39 +1,45 @@
-import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd';
-import { IconGripVertical, IconX } from '@tabler/icons-react';
-import cx from 'clsx';
-import { Text, Box, ActionIcon } from '@mantine/core';
-import { useListState } from '@mantine/hooks';
-import classes from '../styles/DndListHandle.module.css';
-import { useEffect } from 'react';
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
+import { IconGripVertical, IconX } from "@tabler/icons-react";
+import cx from "clsx";
+import { Text, Box, ActionIcon } from "@mantine/core";
+import { useListState } from "@mantine/hooks";
+import classes from "../styles/DndListHandle.module.css";
+import { useEffect } from "react";
 
-
-function DragDropLocations({locations, setLocations}) {
+function DragDropLocations({ locations, setLocations }) {
   const [internalLocations, internalHandlers] = useListState(locations);
 
   // Tkeeps the internal state in sync with external changes to `locations
   useEffect(() => {
     internalHandlers.setState(locations);
-  }, [locations, internalHandlers])
-  
+  }, [locations, internalHandlers]);
+
   const handleRemove = (indexToRemove) => {
-    const newOrder = internalLocations.filter((_, index) => index !== indexToRemove);
+    const newOrder = internalLocations.filter(
+      (_, index) => index !== indexToRemove
+    );
     internalHandlers.setState(newOrder);
     setLocations(newOrder);
-  };;
-
+  };
 
   const items = internalLocations.map((location, index) => (
-    <Draggable key={location.place_id || location.name} index={index} draggableId={location.place_id || location.name}>
+    <Draggable
+      key={location.place_id || `${location.name}-${index}`}
+      index={index}
+      draggableId={location.place_id || `${location.name}-${index}`}
+    >
       {(provided, snapshot) => (
         <div
-          className={cx(classes.item, { [classes.itemDragging]: snapshot.isDragging })}
+          className={cx(classes.item, {
+            [classes.itemDragging]: snapshot.isDragging,
+          })}
           ref={provided.innerRef}
           {...provided.draggableProps}
         >
           <div {...provided.dragHandleProps} className={classes.dragHandle}>
             <IconGripVertical size={18} stroke={1.5} />
           </div>
-          <Box style={{ flexGrow: 1 }}> 
+          <Box style={{ flexGrow: 1 }}>
             <Text fw={500}>{location.name}</Text>
             {location.formatted_address && (
               <Text c="dimmed" size="sm">
@@ -42,7 +48,7 @@ function DragDropLocations({locations, setLocations}) {
             )}
           </Box>
           <ActionIcon
-            variant="transparent" 
+            variant="transparent"
             color="red"
             size="md"
             onClick={() => handleRemove(index)} // remove the location when clicking on X
@@ -72,7 +78,13 @@ function DragDropLocations({locations, setLocations}) {
       <Droppable droppableId="dnd-list" direction="vertical">
         {(provided) => (
           <div {...provided.droppableProps} ref={provided.innerRef}>
-            {items.length > 0 ? items : <Text c="dimmed" ta="center" mt="md">Add locations to your trip!</Text>}
+            {items.length > 0 ? (
+              items
+            ) : (
+              <Text c="dimmed" ta="center" mt="md">
+                Add locations to your trip!
+              </Text>
+            )}
             {provided.placeholder}
           </div>
         )}
