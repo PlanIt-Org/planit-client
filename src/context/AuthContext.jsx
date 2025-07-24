ce613c9da44419de71962ebc0abe81ff2164ae30
thomas <thomas@sibilly.com>
2025-07-23 22:24:21 -0700

    custom hook for sessions

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,43 @@
+// src/context/AuthContext.jsx
+import { createContext, useState, useEffect, useContext } from "react";
+import { supabase } from "../supabaseClient";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [session, setSession] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const getSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      setSession(data.session);
+      setLoading(false);
+    };
+
+    getSession();
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+      }
+    );
+
+    return () => {
+      listener?.unsubscribe();
+    };
+  }, []);
+
+  const value = { session, setSession };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
